fd958610b4658848afb18b8221107b50d46d0e5d
redel-30 <56904995+redel-30@users.noreply.github.com>
2019-11-06 13:45:47 +0800

    Update add.php

--- add.php
+++ add.php
@@ -5,7 +5,6 @@
  
 <body>
 <?php
-//including the database connection file
 include_once("config.php");
  
 if(isset($_POST['Submit'])) {    
@@ -14,7 +13,6 @@ if(isset($_POST['Submit'])) {
     $create = $_POST['created_at'];
     $update = $_POST['updated_at'];
         
-    // checking empty fields
     if(empty($title) || empty($description) || empty($create) || empty($update)) {                
         if(empty($title)) {
             echo "<font color='red'>Title field is empty.</font><br/>";
@@ -30,14 +28,10 @@ if(isset($_POST['Submit'])) {
         if(empty($update)) {
             echo "<font color='red'>Updated at field is empty.</font><br/>";
         }
-        //link to the previous page
         echo "<br/><a href='javascript:self.history.back();'>Go Back</a>";
     } else { 
-        // if all the fields are filled (not empty)             
-        //insert data to database
         $result = mysqli_query($mysqli, "INSERT INTO posts(title,description,created_at,updated_at) VALUES('$title','$description','$create','$update')");
         
-        //display success message
         echo "<font color='green'>Data added successfully.";
         echo "<br/><a href='index.php'>View Result</a>";
     }
