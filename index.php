cdff74566cf86fb566bad684272ee0550c71cd75
redel-30 <56904995+redel-30@users.noreply.github.com>
2019-11-21 22:53:05 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,52 +1,57 @@
-<html>
-<head>
-    <title>Add Data</title>
-</head>
+    <html>
+    <head>
+        <title>Add Data</title>
+    </head>
 
-<body>
-    <br/><br/>
-    
-    <form action="posts.php" method="post" name="form1">
-        <table width="25%" border="0">
+    <body>
+        <br/><br/>
+        
+        <form action="posts.php" method="post" name="form1">
+            <table width="25%" border="0">
 
-            <tr> 
-                <td>Title</td>         
-                <td><input type="text" name="title" value="<?php if (isset($_GET['title'])){ echo $_GET['title']; } ?>"></td>
+                    <tr> 
+                        <td>Title</td>         
+                        <td><input type="text" name="title" value="<?php if (isset($_GET['title'])){ echo $_GET['title']; } ?>"></td>
+                        </tr>
+                        <tr> 
+                        <td>Description </td>
+                        <td><input type="text" name="description" value="<?php if (isset($_GET['description'])){ echo $_GET['description']; }?>"></td>
+                        </tr> 
+                        <td> </td>
+                        <td> </td>
+                        <td> <input type="submit" name="add" value="Add"></td>
+                        <td><input type="hidden" name="id" value="<?php echo $_GET['post_id'];?>"></td>
+                        <td><input type="submit" name="update" value="Update"></td>
+                    </tr>
+            </table>
+        </form>
+        
+        <title>Homepage</title>
+    </head>
+    </body>
+       
+
+
+
+
+       
+        <table width='80%' border=0>
+            <tr bgcolor='#CCCCCC'>
+                <td>Title</td>
+                <td>Description</td>
+                <td>Option</td>
             </tr>
-            <tr> 
-                <td>Description </td>
-                <td><input type="text" name="description" value="<?php if (isset($_GET['description'])){ echo $_GET['description']; }?>"></td>
-            </tr> 
-                <td> </td>
-                <td> </td>
-                <td> <input type="submit" name="add" value="Add"></td>
-                <td><input type="hidden" name="id" value="<?php echo $_GET['post_id'];?>"></td>
-                <td><input type="submit" name="update" value="Update"></td>
-            </tr>
+            <?php 
+            include_once("config.php");
+            $result = mysqli_query($mysqli, "SELECT * FROM posts ORDER BY id DESC");
+            while($res = mysqli_fetch_array($result)) {         
+                echo "<tr>";
+                echo "<td>".$res['title']."</td>";
+                echo "<td>".$res['description']."</td>";  
+                echo "<td><a href=\"posts.php?post_id=$res[id]\">Edit</a> 
+                |<a href=\"posts.php?delete_posts=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";   
+            }    
+            ?>
         </table>
-    </form>
-    
-    <title>Homepage</title>
-</head>
-</body>
-
-    <table width='80%' border=0>
-        <tr bgcolor='#CCCCCC'>
-            <td>Title</td>
-            <td>Description</td>
-            <td>Option</td>
-        </tr>
-        <?php 
-        include_once("config.php");
-        $result = mysqli_query($mysqli, "SELECT * FROM posts ORDER BY id DESC");
-        while($res = mysqli_fetch_array($result)) {         
-            echo "<tr>";
-            echo "<td>".$res['title']."</td>";
-            echo "<td>".$res['description']."</td>";  
-            echo "<td><a href=\"posts.php?post_id=$res[id]\">Edit</a> 
-            |<a href=\"posts.php?delete_posts=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";   
-        }    
-        ?>
-    </table>
-</body>
-</html>    
+    </body>
+    </html>    
--- posts.php
+++ posts.php
@@ -1,56 +1,71 @@
-<?php
-include_once("config.php");
+    <?php
+    include_once("config.php");
 
-if(isset($_POST['add'])) {    
-    $title = $_POST['title'];
-    $description = $_POST['description'];
+    //add
 
-    $result = mysqli_query($mysqli, "INSERT INTO posts(title,description) VALUES('$title','$description')");
+    if(isset($_POST['add'])) {    
+        $title = $_POST['title'];
+        $description = $_POST['description'];
+        $result = mysqli_query($mysqli, "INSERT INTO posts(title,description) VALUES('$title','$description')");
+        header("location: index.php?msg=successfully updates");
+
+    }
 
 
-    header("location: index.php?msg=successfully updates");
+    if(isset($_GET['post_id'])) {  
 
-}
-        if(isset($_GET['post_id'])) {  
+       $post_id = $_GET['post_id'];
 
-         $post_id = $_GET['post_id'];
 
-    $result = mysqli_query($mysqli, "SELECT * FROM posts WHERE id=$post_id");
-    while($res = mysqli_fetch_array($result)){
-     $title = $res['title'];
-     $description = $res['description'];
-     $create = $res['created_at'];
-     $update = $res['updated_at']; 
-     $id = $res['id'];
-     header("location: index.php?edit=1&post_id=".$id."&title=".$title."&description=".$description);
-     
-      }
-}
+       $result = mysqli_query($mysqli, "SELECT * FROM posts WHERE id=$post_id");
+       while($res = mysqli_fetch_array($result)){
+           $title = $res['title'];
+           $description = $res['description'];
+           $create = $res['created_at'];
+           $update = $res['updated_at']; 
+           $id = $res['id'];
+           header("location: index.php?edit=1&post_id=".$id."&title=".$title."&description=".$description);
+
+       }
+    }
+
+
     if(isset($_POST['update'])){   
     $id = $_POST['id'];
-        
+
     $title=$_POST['title'];
+
     $description=$_POST['description'];
+
     $id = $_POST['id'];
-    
+
     $title=$_POST['title'];
+
     $description=$_POST['description'];
+    
     $result = mysqli_query($mysqli, "UPDATE posts SET title='$title',description='$description'WHERE id=$id");
     header("Location: index.php");}
     $result = mysqli_query($mysqli, "SELECT * FROM posts WHERE id=$id");
     while($res = mysqli_fetch_array($result)){
-    $title = $res['title'];
-    $description = $res['description'];
-}
+        $title = $res['title'];
+        $description = $res['description'];
+    }
 
-if (isset($_GET['delete_posts']))
-{
-    $delete_posts = $_GET['delete_posts'];
-    
-    $result = mysqli_query($mysqli, "DELETE FROM posts WHERE id=$delete_posts");
-    
-    header("Location:index.php");
 
-}
-?>
+
+
+    if (isset($_GET['delete_posts']))
+
+    {
+        $delete_posts = $_GET['delete_posts'];
+        
+        $result = mysqli_query($mysqli, "DELETE FROM posts WHERE id=$delete_posts");
+        
+        header("Location:index.php");
+
+    }
+
+
+
+    ?>
 
